ce0182b3baf2699da83dcbf35d5c8dac6d7ae73e
TazyyNguu <envyava25@gmail.com>
2026-04-03 10:41:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import joblib
-import numpy as np
 
 app = Flask(__name__)
-CORS(app)
+
+# CHO PHÉP TẤT CẢ DOMAIN
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 model = joblib.load("model.pkl")
 encoders = joblib.load("encoders.pkl")
